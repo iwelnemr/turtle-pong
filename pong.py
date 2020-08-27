7a09736da2c89cd012d732f10db7998dc272a204
iwelnemr <>
2020-08-27 17:29:25 +0200

    Update pong.py

--- pong.py
+++ pong.py
@@ -2,3 +2,23 @@ import turtle
 wn = turtle.Screen()
 wn.bgcolor("light blue")
 wn.title("Pong")
+wn.setup(800, 600)
+windowWidth = 800
+windowHeight = 600
+
+class bat:
+    pass
+
+class bat2:
+    pass
+
+class ball:
+    pass
+
+bat = bat()
+bat2 = bat2()
+ball = ball()
+
+
+while True:
+    wn.update()
