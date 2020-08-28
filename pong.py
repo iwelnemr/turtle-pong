461eae59f5af20d7b49b67ba54d1973f7a6935bd
iwelnemr <>
2020-08-28 11:02:29 +0200

    Update bat, bat2 and ball classes

--- pong.py
+++ pong.py
@@ -3,17 +3,49 @@ wn = turtle.Screen()
 wn.bgcolor("light blue")
 wn.title("Pong")
 wn.setup(800, 600)
+wn.tracer(0)
 windowWidth = 800
 windowHeight = 600
 
 class bat:
-    pass
+    bat = turtle.Turtle()
+    bat.penup()
+    bat.shape("square")
+    bat.setx(0)
+    bat.sety(-250)
+    bat.shapesize(1,5)
+    bat.color("black")
+
+    def update(self):
+        pass
+
 
 class bat2:
-    pass
+    bat2 = turtle.Turtle()
+    bat2.penup()
+    bat2.shape("square")
+    bat2.setx(0)
+    bat2.sety(250)
+    bat2.shapesize(1,5)
+    bat2.color("black")
+
+    def update(self):
+        pass
 
 class ball:
-    pass
+    ball = turtle.Turtle()
+    ball.penup()
+    ball.shape("circle")
+    ball.setx(0)
+    ball.sety(0)
+    ball.color("black")
+
+    def update(self):
+        pass
+
+class input:
+    def update(self):
+        pass
 
 bat = bat()
 bat2 = bat2()
